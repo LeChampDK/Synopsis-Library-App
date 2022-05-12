4e7e38e87435938b0ef006d0287f9547b641c521
René Jørgensen <rene686f@easv365.dk>
2022-05-12 13:21:42 +0200

    Update

---
+++ Global/Models/UserExistCheck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Global.Models
+{
+    public class UserExistCheck
+    {
+        public int UserId { get; set; }
+    }
+}
---
+++ Rental/Models/RentalStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Rental.Models
+{
+    public class RentalStatus
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public bool Rented { get; set; }
+        public bool Reserved { get; set; }
+        public DateTime ReservedTime { get; set; }
+    }
+}
